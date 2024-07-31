75c2697113454626941670e9f9546aeed31654a9
Heinz Burgstaller <heinz.burgstaller@dynatrace.com>
2024-07-31 08:12:18 +0200

    header param

--- app/routes/$name.tsx
+++ app/routes/$name.tsx
@@ -1,13 +1,16 @@
 import { json, LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
-export async function loader({ params }: LoaderFunctionArgs) {
-    return json({ name: params.name || '' });
+export async function loader({ params, request }: LoaderFunctionArgs) {
+    return json({ name: params.name || '', url: request.url });
 }
 
 const Dynamic = () => {
-    const { name } = useLoaderData<typeof loader>();
-    return (<div>You are at: {name}</div>);
+    const { name, url } = useLoaderData<typeof loader>();
+    return (<div>
+        <div>Name: {name}</div>
+        <div>URL: {url}</div>
+    </div>);
 }
 
 export default Dynamic;
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,8 +9,12 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-
 export async function loader({ request }: LoaderFunctionArgs) {
+  const specialHeader = request.headers.get("X-Special-Header");
+  if (specialHeader) {
+    return json({ special: "You are special: " + specialHeader, todos: null });
+  }
+
   const { supabase, headers } = createSupabaseServerClient(request);
   const { data, error } = await supabase.auth.getSession();
   if (error) {
@@ -23,11 +27,16 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
   console.log(data.session.user.id)
   const { data: todos } = await supabase.from('todos').select('*').order('desc', { ascending: true });
-  return json(todos, { headers })
+  return json({ todos, special: null }, { headers })
 }
 
 export default function Index() {
-  const todos = useLoaderData<typeof loader>();
+  const { todos, special } = useLoaderData<typeof loader>();
+
+  if (special) {
+    return <div>{special}</div>;
+  }
+
   console.log(todos)
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
